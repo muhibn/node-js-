66ca851f9e30f5a6f8bb9fa71d3803e64a20700b
Muhibullah <mohebullah2015@gmail.com>
2023-07-24 00:34:27 +0530

    add the error function in the code lc 19

--- react nodejs/react-ii/src/component/blog.js
+++ react nodejs/react-ii/src/component/blog.js
@@ -4,8 +4,31 @@ import Bloglist from "./bloglist";
 const Blog = () => {
     const [blog, setBlog] = useState(null);
     let [name ,nameState]=useState("Muhibullah");
+
+     const [isPending ,setPending]=useState(true);
+     const [error ,setError]=useState(null);
+
     useEffect(()=>{
-        fetch('http://localhost:8000/blogs').then(res=>{ return res.json()}).then(data=>{console.log(data);setBlog(data); })
+
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogss').then(res=>{
+                console.log(res);
+                if(!res.ok)
+                {
+                    throw Error("The servers is not exist ");
+
+                }
+                return res.json()}).then(data=>{
+               console.log(data);setBlog(data); 
+               setPending(false);
+               }).catch(err => 
+                {
+                    setPending(false);
+                    console.log(err.message);
+                    setError(err.message);
+                })
+            
+        }, 100);
 
     } ,[] )
 
@@ -17,10 +40,13 @@ const Blog = () => {
     const handleDelete = (id) => {
         const newBlog = blog.filter(blogItem => blogItem.id !== id);
         setBlog(newBlog);
+
     }
 
     return (
         <div className="container">
+            {error && <div>{error}</div>}
+           { isPending && <div>Loading..</div>}
            {blog &&  <Bloglist blogs={blog} handleDelete={handleDelete} /> }
             <button  onClick={()=>changeName("Ezatullah")}>change name </button>
             <h1>{name}</h1>
